1c6311222bc0f8320e45e7d0e5b58306af2661e2
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2019-06-01 12:48:21 -0300

    Corrida entera de a.i hasta a.v

--- principal.R
+++ principal.R
@@ -2,6 +2,8 @@ library('tidyverse')
 library('stats')
 set.seed(42)
 
+# Coeficientes "platonicos" (i.e., del proceso generador de datos)
+beta_pgd <- c(4, 2, -3, 0.5, 0)
 
 generadores <- list(
     "x1" = function(n) { runif(n, min=-5, max=5) },
@@ -10,7 +12,7 @@ generadores <- list(
     "x4" = function(n) { runif(n, min=-5, max=5) },
     "eps" = function(n) { rexp(n, rate = 1/2) - 2 },
 #    "eps2" = function(n) { rchisq(n, 3) - 3 },
-    "y" = function(x1, x2, x3, x4, eps, ...) { 4 + 2*x1 - 3*x2 + 0.5*x3 + 0*x4 + eps }
+    "y" = function(x1, x2, x3, x4, eps, ...) { c(1, x1, x2, x3, x4) %*% beta_pgd + eps }
 )
 
 generar_muestra <- function(n, generadores) {
@@ -28,16 +30,10 @@ generar_muestra <- function(n, generadores) {
   return(df)
 }
 
-n_test <- 10
-muestra <- generar_muestra(n_test, generadores)
-llamada_lm <- lm(y ~ x1 + x2 + x3 + x4, muestra)
-
-vcov(llamada_lm)
-
 intervalo_conf <- function(a_vec, llamada_lm, alfa, metodo = "exacto") {
   
   betahat <- llamada_lm$coefficients
-  # Matrix de covarianza estimada para los coeficientes
+  # Matriz de covarianza estimada para los coeficientes
   Sigmahat <- vcov(llamada_lm)
   
   n_muestra <- nrow(llamada_lm$model)
@@ -56,6 +52,20 @@ intervalo_conf <- function(a_vec, llamada_lm, alfa, metodo = "exacto") {
   return(c(centro - delta, centro + delta))
 }
 
-alfa <- 0.05
-a_vec <- c(0,0,0,0,1)
-intervalo_conf(a_vec, llamada_lm, alfa, metodo = "exacto")
+cubre <- function(intervalo, valor) { intervalo[1] <= valor & intervalo[2] >= valor}
+
+n_test <- 6
+muestra <- generar_muestra(n_test, generadores)
+llamada_lm <- lm(y ~ x1 + x2 + x3 + x4, muestra)
+alfa <- 0.10
+a1 <- c(0,1,0,0,0)
+ic1_ex <- intervalo_conf(a1, llamada_lm, alfa, metodo = "exacto")
+cubre(ic1_ex, beta_pgd[2])
+ic1_as <- intervalo_conf(a1, llamada_lm, alfa, metodo = "asintotico")
+cubre(ic1_as, beta_pgd[2])
+a4 <- c(0,0,0,0,1)
+ic4_ex <- intervalo_conf(a4, llamada_lm, alfa, metodo = "exacto")
+cubre(ic4_ex, beta_pgd[5])
+ic4_as <- intervalo_conf(a4, llamada_lm, alfa, metodo = "asintotico")
+cubre(ic4_as, beta_pgd[5])
+
